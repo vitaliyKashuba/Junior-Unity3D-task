f1f8a514806b12d92762801b99f8e93a3ebca9a2
vitaliyKashuba <k.vitaliy1994@gmail.com>
2017-01-21 18:34:53 +0200

    add zombie and simple (random movement) AI

---
+++ Junior-Unity3D-task/Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	protected static float basicSpeed = 0.3f;
+	protected static float speedIncreaser = 0.05f;
+	protected EnemyState state = EnemyState.RANDOM_WALK;
+	protected bool isFacingRight = false;
+	protected float moveHorizontal, moveVertical = 0;
+
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	protected void FixedUpdate()
+	{
+		Debug.Log ("enemy fix update");
+		if (moveHorizontal > 0 && !isFacingRight) 
+		{
+			Flip ();
+		} 
+		else if (moveHorizontal < 0 && isFacingRight) 
+		{
+			Flip ();
+		}
+	}
+
+	protected void Flip()
+	{
+		Debug.Log ("flip");
+		isFacingRight = !isFacingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	//findPlayer(), goToPlayer..
+}
---
+++ Junior-Unity3D-task/Assets/Scripts/Zombie.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : Enemy 
+{
+
+	private float speed = basicSpeed + speedIncreaser;
+	private Animator animator;
+	private Direction direction;
+
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	void Update () 
+	{
+		switch (state)
+		{
+			case EnemyState.RANDOM_WALK:
+				if (Randomizer.getBooleanRandom (1)) 
+				{
+					doWait ();
+				} 
+				else 
+				{
+					doWalk ();
+				}
+				break;
+			case EnemyState.PURSUIT:
+				//TODO add seek and destroy logic
+				break;
+		}
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (moveHorizontal * speed, moveVertical * speed);
+		if (moveHorizontal != 0 || moveVertical != 0) 
+		{
+			animator.SetBool ("isWalk", true);
+		} else 
+		{
+			animator.SetBool ("isWalk", false);
+		}
+	}
+
+	void doWait()
+	{
+		Debug.Log ("doWait");
+		moveVertical = moveHorizontal = 0;
+	}
+
+	void doWalk()
+	{
+		if (Randomizer.getBooleanRandom (10)) 
+		{
+			changeDirection ();
+		}
+		Debug.Log ("doWalk" + direction);
+		//GetComponent<Rigidbody2D>().velocity = new Vector2 (moveHorizontal * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+	void changeDirection()
+	{
+		direction = Randomizer.getRandomDirection ();
+		switch (direction) 
+		{
+		case Direction.NORTH:
+			moveVertical=2;
+			break;
+		case Direction.SOUTH:
+			moveVertical=-1;
+			break;
+		case Direction.EAST:
+			moveHorizontal=1;
+			break;
+		case Direction.WEST:
+			moveHorizontal=-1;
+			break;
+		}
+	}
+
+	void doAttack()
+	{
+	}		
+
+	void OnTriggerEnter2D(Collider2D c) 
+	{
+		Debug.Log ("trigger enter");
+		if (c.gameObject.CompareTag ("Player")) 
+		{
+			animator.SetBool ("isAttack", true);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D c)
+	{
+		Debug.Log ("trigger exit");
+		animator.SetBool ("isAttack", false);
+	}
+}
